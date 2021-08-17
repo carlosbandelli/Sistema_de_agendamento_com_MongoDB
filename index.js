850bbedb32fb15863db134f07095cb1dfda34c85
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-08-17 19:38:57 -0300

    Bibliotecas instaladas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const appointmentService = require("./services/appointmentService")
 
 app.use(express.static("public"));
 
@@ -14,5 +15,26 @@ mongoose.connect("mongodb://localhost:27017/agendamento",{useNewUrlParser: true,
 app.get("/", (req, res) => {
     res.send("Oi!");
 });
+
+app.get("/cadastro",(req, res) => {
+    res.render("create")
+})
+
+app.post("/create", async(req, res)=> {
+
+    var status = await appointmentService.Create(
+        req.body.name,
+        req.body.email,
+        req.body.cpf,
+        req.body.date,
+        req.body.time,
+        req.body.decription
+        )
+        if(status){
+            res.redirect("/")
+        }else{
+            res.send("Ocorreu uma falha!")
+        }
+})
 
 app.listen(8080, () => {});
